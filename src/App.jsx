c19b2d8f6bb3a033febefb6d244476bad8447bb8
Mohammad Azril Sugiarto <azrilsugiarto06@gmail.com>
2026-05-13 21:08:18 +0700

    fix: enhance console logging in PrivateRoute and HomeRedirect functions

--- src/App.jsx
+++ src/App.jsx
@@ -23,13 +23,12 @@ import PurchaseReturnsPage from "./pages/inventory/purchase-returns/returns";
 
 function PrivateRoute() {
   const { isAuthenticated } = useAuthStore();
-  console.log(isAuthenticated)
+  console.log(isAuthenticated, "MASU")
   return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
 }
 
 function HomeRedirect() {
   const { user } = useAuthStore();
-  console.log(user) 
   if (!user) return <Navigate to="/login" replace />;
   return user.role === CASHIER ? (
     <Navigate to="/pos" replace />
