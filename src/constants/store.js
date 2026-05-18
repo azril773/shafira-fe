cbf909070f2712e0fa4bb76f0b43cd62b7935cb9
Mohammad Azril Sugiarto <azrilsugiarto06@gmail.com>
2026-05-19 02:22:15 +0700

    fix: update store address and phone number for accuracy

--- src/constants/store.js
+++ src/constants/store.js
@@ -1,3 +1,3 @@
 export const STORE_NAME = 'ShafiraMart'
-export const STORE_ADDRESS = 'Jl. Contoh No. 1, Kota'
-export const STORE_PHONE = '0812-3456-7890'
+export const STORE_ADDRESS = 'Jl. Raya Parungponteng, Kabupaten Tasikmalaya'
+export const STORE_PHONE = '0853-5194-4873'
